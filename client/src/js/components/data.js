e2c53a183019ad6cb4ed3f6eb474d19edd3741d3
dotexe0 <dotexe0@gmail.com>
2017-05-19 15:48:02 -0700

    added components to manage data

---
+++ client/src/js/components/data.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    "title": "Business 1",
+    "subtitle": "1 N. San Antonio Road",
+    "avatar": 10,
+    "position": [37.381660, -122.114099],
+    "img": "https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?w=940&h=650&auto=compress&cs=tinysrgb"
+  },
+  {
+    "title": "Business 2",
+    "subtitle": "South",
+    "avatar": 5,
+    "position": [37.380367, -122.113953],
+    "img": "https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?w=940&h=650&auto=compress&cs=tinysrgb"
+  },
+  {
+    "title": "Business 3",
+    "subtitle": "North",
+    "avatar": 4,
+    "position": [37.388716, -122.114050],
+    "img": "https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?w=940&h=650&auto=compress&cs=tinysrgb"
+  }
+]
