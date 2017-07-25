09d72e2721785ee933659946c20fa9d924b03a9c
Daniel Varela <dotexe0@gmail.com>
2017-07-24 22:18:29 -0700

    fixed placecard error with muithemeprovider

--- client/src/js/components/PlaceCard.js
+++ client/src/js/components/PlaceCard.js
@@ -1,30 +1,34 @@
 import React, { Component } from 'react';
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 
 class PlaceCard extends Component {
   render() {
       const { name, location, position, img, rating } = this.props;
     return (
-    <Card>
-      <CardHeader
-        title={ name }
-        subtitle={ location }
-        avatar={ rating }
-      />
-        <img src={ img } />
-      <CardText>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-        Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-        Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-      </CardText>
-      <CardActions>
-        <FlatButton label="Website" />
-        <FlatButton label="Directions" />
-      </CardActions>
-    </Card>
+      <MuiThemeProvider>
+        <Card>
+          <CardHeader
+            title={ name }
+            subtitle={ location }
+            avatar={ rating }
+          />
+            <img src={ img } />
+          <CardText>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+            Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+            Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+          </CardText>
+          <CardActions>
+            <FlatButton label="Website" />
+            <FlatButton label="Directions" />
+          </CardActions>
+        </Card>
+      </MuiThemeProvider>
+
     )
   }
 
